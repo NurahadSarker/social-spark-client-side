93e88326f622d02731dc200853b4f485d11b9bcd
NurahadSarker <nurahadrobi451@gmail.com>
2025-11-13 21:14:46 +0600

    added up coming page function

--- src/Pages/UpComingEventPage.jsx
+++ src/Pages/UpComingEventPage.jsx
@@ -8,18 +8,22 @@ const UpComingEventPage = () => {
     useEffect(() => {
         fetch(`http://localhost:5000/events?search=${searchTerm}`)
             .then(res => res.json())
-            .then(data => setEvents(data))
+            .then(data => {
+                const today = new Date();
+                const upcomingEvents = data.filter(event => new Date(event.date) >= today);
+                setEvents(upcomingEvents);
+            })
     }, [searchTerm]);
 
     return (
         <div className="max-w-[1200px] mx-auto py-10">
             <h1 className='text-5xl font-bold text-center text-[#29B467] mb-10'>Up Coming Event</h1>
             <div className="text-center mb-2">
-                <input 
-                    type="text" 
-                    placeholder="Search event by title..." 
-                    value={searchTerm} 
-                    onChange={(e) => setSearchTerm(e.target.value)} 
+                <input
+                    type="text"
+                    placeholder="Search event by title..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                     className="border border-gray-400 px-4 py-2 rounded w-1/2 focus:outline-none focus:ring-2 focus:ring-green-400"
                 />
             </div>
