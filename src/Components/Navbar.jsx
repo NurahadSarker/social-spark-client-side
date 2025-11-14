da3ce5608b06fc70b7bb282cd50a6680870ed366
NurahadSarker <nurahadrobi451@gmail.com>
2025-11-14 12:50:11 +0600

    added new toggle button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         localStorage.setItem("theme", theme)
     }, [theme])
 
-    const handleTheme = (checked) =>{
+    const handleTheme = (checked) => {
         setTheme(checked ? "dark" : "light")
     }
 
@@ -62,11 +62,21 @@ const Navbar = () => {
                         </ul>
                     </div>
                     <div className="navbar-end">
-                        <input
+                        {/* <input
                             onChange={(e) => handleTheme(e.target.checked)}
                             type="checkbox"
                             defaultChecked={localStorage.getItem('theme') === "dark"}
-                            className="toggle mr-2" />
+                            className="toggle mr-2" /> */}
+                        <label className="toggle text-base-content mr-2">
+                            <input onChange={(e) => handleTheme(e.target.checked)}
+                            type="checkbox"
+                            defaultChecked={localStorage.getItem('theme') === "dark"} className="theme-controller mr-2" />
+
+                            <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
+
+                            <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
+
+                        </label>
                         <div className="dropdown dropdown-end mr-2">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
