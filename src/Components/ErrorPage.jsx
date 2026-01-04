877429bc5a2f947702de7779752930c31ef54847
NurahadSarker <nurahadrobi451@gmail.com>
2026-01-04 10:50:06 +0600

    added some changes

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <p>Error Page</p>
+        <div className='flex flex-col items-center justify-center min-h-screen'>
+            <h1 className='text-[150px] font-bold'>404</h1>
+            <p className='text-[32px]'>Page Not Found</p>
         </div>
     );
 };
